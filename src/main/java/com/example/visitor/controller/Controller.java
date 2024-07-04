7ec57273f3a282148daf2b61fbb30ba2339d714a
Omomurewa George-Ashiru <rachelleashiru@gmail.com>
2024-07-04 15:35:28 +0100

    location

--- src/main/java/com/example/visitor/controller/Controller.java
+++ src/main/java/com/example/visitor/controller/Controller.java
@@ -1,6 +1,7 @@
 package com.example.visitor.controller;
 
 import com.example.visitor.dto.ResponseDto;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.BadRequestException;
@@ -12,46 +13,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerErrorException;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.security.InvalidKeyException;
+
+import static com.example.visitor.utils.HelperUtils.getClientCity;
 import static com.example.visitor.utils.HelperUtils.getClientIp;
 
 @RestController
 @RequiredArgsConstructor
 public class Controller {
     @GetMapping(value = "/api/hello", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseDto> getVisitorGreeting(@RequestParam String name, HttpServletRequest httpServletRequest) throws ServerErrorException {
+    public ResponseEntity<ResponseDto> getVisitorGreeting(@RequestParam String name, HttpServletRequest httpServletRequest) throws ServerErrorException, IOException, GeoIp2Exception, InvalidKeyException {
 
         if(name == null){
             return new ResponseEntity<>(new ResponseDto(), HttpStatus.BAD_REQUEST);
         }
-        var response = new ResponseDto(getClientIp(httpServletRequest), "New York", "Hello, "+name+"!, the temperature is 11 degrees Celcius in New York");
+        var location = getClientCity(httpServletRequest);
+        var response = new ResponseDto(getClientIp(httpServletRequest), location, "Hello, "+name+"!, the temperature is 11 degrees Celcius in "+ location);
         return new ResponseEntity<>(response, HttpStatus.OK);
 
     }
-
-//    private static HttpServletRequest request;
-//
-//    @Autowired
-//    public void setRequest(HttpServletRequest request) {
-//        this.request = request;
-//    }
-//    public static String getClientIp() {
-//        String remoteIp = "";
-//
-//        if (request != null) {
-//            remoteIp = request.getHeader("X-FORWARDED-FOR");
-//        }
-//        if ((remoteIp == null || "".equals(remoteIp)) && request != null) {
-//            remoteIp = request.getRemoteAddr();
-//        }
-//
-//        //clean up Ip
-//        remoteIp = checkIp(remoteIp);
-//        return remoteIp;
-//    }
-//    public static String checkIp(String ipAdd) {
-//        if (ipAdd.contains(",")) {
-//            ipAdd = ipAdd.split(",")[0].trim();
-//        }
-//        return ipAdd;
-//    }
 }
--- src/main/java/com/example/visitor/utils/HelperUtils.java
+++ src/main/java/com/example/visitor/utils/HelperUtils.java
@@ -1,8 +1,24 @@
 package com.example.visitor.utils;
 
+import com.google.code.geocoder.Geocoder;
+import com.maxmind.geoip2.DatabaseReader;
+import com.maxmind.geoip2.WebServiceClient;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
+import com.maxmind.geoip2.model.CityResponse;
+import com.maxmind.geoip2.model.CountryResponse;
+import com.maxmind.geoip2.model.InsightsResponse;
+import com.maxmind.geoip2.record.City;
+import com.maxmind.geoip2.record.Country;
 import jakarta.servlet.http.HttpServletRequest;
+import ua_parser.Client;
+import ua_parser.Parser;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.security.InvalidKeyException;
 
 public class HelperUtils {
+
     public static String getClientIp(HttpServletRequest request) {
         String clientIp = "";
 
@@ -16,4 +32,21 @@ public class HelperUtils {
 
         return clientIp;
     }
+
+
+
+    public static String getClientCity(HttpServletRequest request) throws IOException, GeoIp2Exception, InvalidKeyException{
+        String clientIp = getClientIp(request);
+        File database = new File("src/main/resources/GeoLite2-City.mmdb");
+        DatabaseReader reader = new DatabaseReader.Builder(database).build();
+
+        InetAddress ipAddress = InetAddress.getByName(clientIp);
+
+        CityResponse response = reader.city(ipAddress);
+
+        City city = response.getCity();
+
+        return city.getName();
+    }
+
 }
